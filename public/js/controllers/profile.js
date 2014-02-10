fd3be4714ac224537b7814110aa678ef55d76636
Xingjia <xzhang009@e.ntu.edu.sg>
2014-02-10 10:07:55 +0800

    shows pending state for CODE name

--- public/js/controllers/profile.js
+++ public/js/controllers/profile.js
@@ -5,13 +5,13 @@ angular.module('ace.system')
     $scope.global = Global;
     $scope.urlUserName = $routeParams.username;
     $scope.username = Global.user.name;
-    $scope.codename = $scope.username;
+    $scope.codename = (Global.user.codeName === null) ? Global.user.name : (Global.user.isManufacturer ? Global.user.codeName : Global.user.codeName + ' (pending)');
     $scope.codeNameInput = {value:''};
     $scope.email = Global.user.email;
     $scope.editable = false;
     $scope.codeNameMinLength= 1;
     $scope.codeNameMaxLength= 30;
-    $scope.codeInfo = 'Code name is optional information only applicable for manufacturers.Code name appears in the field "manufacturer" in your catalog';
+    $scope.codeInfo = 'Code name is optional information only applicable for manufacturers.Code name appears in the field "manufacturer" in your catalog. By changing your default CODE name, you automatically apply to register as an manufacturer. You new CODE name is only confirmed after the verfication from the administrator.';
     $scope.codeInfoTitle = 'What is CODE name?';
     $scope.toggleEdit = function(){
         $scope.editable= !$scope.editable;
