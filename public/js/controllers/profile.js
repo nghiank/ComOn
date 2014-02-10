90520bd72bfdf429c7adcd322de4b66031d5b653
Xingjia <xzhang009@e.ntu.edu.sg>
2014-02-10 18:27:25 +0800

    profile page redirect to index page if user not authenticated

--- public/js/controllers/profile.js
+++ public/js/controllers/profile.js
@@ -1,32 +1,41 @@
 'use strict';
 
 angular.module('ace.system')
-.controller('ProfileController', ['$scope', 'Global', '$routeParams', '$resource', '$location', function ($scope, Global, $routeParams, $resource, $location) {
-    $scope.global = Global;
-    $scope.urlUserName = $routeParams.username;
-    $scope.username = Global.user.name;
-    $scope.codename = (Global.user.codeName === null) ? Global.user.name : (Global.user.isManufacturer ? Global.user.codeName : Global.user.codeName + ' (pending)');
-    $scope.codeNameInput = {value:'', valid:false,minLength:1, maxLength:30, pattern:/^\s*\w*\s*$/ };
-    $scope.email = Global.user.email;
-    $scope.editable = false;
+.controller('ProfileController', ['$scope', 'Global', '$routeParams', '$resource', '$location',function ($scope, Global, $routeParams, $resource, $location) {
+
+    $scope.init = function(){
+        if (!Global.authenticated)
+           window.location.replace('/');
+        $scope.global = Global;
+        $scope.urlUserName = $routeParams.username;
+        $scope.username = Global.user.name;
+        $scope.codename = (Global.user.codeName === null) ? Global.user.name : (Global.user.isManufacturer ? Global.user.codeName : Global.user.codeName + ' (pending)');
+        $scope.codeNameInput = {value:'', valid:false,minLength:1, maxLength:30, pattern:/^\s*\w*\s*$/ };
+        $scope.email = Global.user.email;
+        $scope.editable = false;
+
+        $scope.toggleEdit = function(){
+        $scope.editable= !$scope.editable;
+        };
+        $scope.updateCodeName = function(){
+            $scope.editable= !$scope.editable;
+            var update = $resource('/updateCodeName/:name/:codeName',
+                                    {name:Global.user.name, codeName: $scope.codeNameInput.value},
+                                    {change:{method:'GET'}});
+            update.change(function(){
+                Global.user.codeName = $scope.codeNameInput.value;
+                $scope.codename = (Global.user.codeName === null) ? Global.user.name : (Global.user.isManufacturer ? Global.user.codeName : Global.user.codeName + ' (pending)');
+            });
+        };
+        $scope.checkValid = function(){
+            $scope.lengthCheck = $scope.codeNameInput.value.length >= $scope.codeNameInput.minLength && $scope.codeNameInput.value.length <= $scope.codeNameInput.maxLength ;
+            $scope.patternCheck = $scope.codeNameInput.pattern.test($scope.codeNameInput.value);
+            $scope.codeNameInput.valid = ($scope.lengthCheck && $scope.patternCheck);
+        };
+    };
+    $scope.init();
     
     
-    $scope.toggleEdit = function(){
-        $scope.editable= !$scope.editable;
-    };
-    $scope.updateCodeName = function(){
-        $scope.editable= !$scope.editable;
-        var update = $resource('/updateCodeName/:name/:codeName',
-                                {name:Global.user.name, codeName: $scope.codeNameInput.value},
-                                {change:{method:'GET'}});
-        update.change(function(){
-            Global.user.codeName = $scope.codeNameInput.value;
-            $scope.codename = (Global.user.codeName === null) ? Global.user.name : (Global.user.isManufacturer ? Global.user.codeName : Global.user.codeName + ' (pending)');
-        });
-    };
-    $scope.checkValid = function(){
-        if ($scope.codeNameInput.value.length >= $scope.codeNameInput.minLength && $scope.codeNameInput.value.length <= $scope.codeNameInput.maxLength && $scope.codeNameInput.pattern.test($scope.codeNameInput.value))
-        $scope.codeNameInput.valid = true;
-    }
+
 }])
 ;
