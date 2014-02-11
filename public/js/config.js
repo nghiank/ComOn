9d695a4e9c541988e158715540d6a41f104b5c8d
Xingjia <xzhang009@e.ntu.edu.sg>
2014-02-11 11:09:51 +0800

    put locale translation data into a separate json file 'en.json'

--- public/js/config.js
+++ public/js/config.js
@@ -29,8 +29,9 @@ angular.module('ace').config(['$locationProvider',
 //Setting translate Dictionary
 angular.module('ace')
 .config(['$translateProvider', function($translateProvider) {
-    $translateProvider.translations({
-        'CODE_NAME_INFO_TITLE': 'What is CODE name?',
-        'CODE_NAME_INFO':'Code name is optional information only applicable for manufacturers.Code name appears in the field "manufacturer" in your catalog. By changing your default CODE name, you automatically apply to register as an manufacturer. Your new CODE name is only confirmed after the verfication from the administrator.'
-    });
+    $translateProvider.preferredLanguage('en');
+    $translateProvider.useStaticFilesLoader({
+        prefix: '/js/',
+        suffix: '.json'
+    });    
 }]);
