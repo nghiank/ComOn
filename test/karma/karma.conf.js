9d359e1f41240a38806f127992f31e0374f47343
Xingjia <xzhang009@e.ntu.edu.sg>
2014-02-13 10:42:30 +0800

    modified karma config file for translate-loader-partial.

--- test/karma/karma.conf.js
+++ test/karma/karma.conf.js
@@ -22,7 +22,7 @@ module.exports = function(config) {
             'public/lib/angular-resource/angular-resource.js',
             'public/lib/angular-route/angular-route.js',
             'public/lib/angular-translate/angular-translate.js',
-            'public/lib/angular-translate-loader-static-files/angular-translate-loader-static-files.js',
+            'public/lib/angular-translate-loader-partial/angular-translate-loader-partial.js',
             'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
             'public/lib/angular-bootstrap/ui-bootstrap.js',
             'public/lib/angular-ui-utils/ui-utils.js',
@@ -35,7 +35,8 @@ module.exports = function(config) {
             'public/js/controllers/header.js',
             'public/js/init.js',
             'test/karma/unit/**/*.js',
-            'test/karma/mock/translate.js'
+            'test/karma/mock/translate.js',
+            {pattern: 'public/locale/*/*.json', included:false}
         ],
 
 
