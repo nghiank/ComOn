61d6da32b550485c5452474dd084146154266ef8
Akaash Gupta <akaash.gupta@autodesk.com>
2014-03-17 11:08:35 +0800

    Added check against republishing same version.

--- public/js/controllers/versionListCtrl.js
+++ public/js/controllers/versionListCtrl.js
@@ -14,6 +14,8 @@ angular.module('ace.schematic')
     $scope.publishComponent = function(number) {
         if(!number)
             number = 0;
+        if(number === $scope.target.published)
+            return;
         $scope.formValidator.checkSchematicNodeName($scope.versionInfo.versions[number? (number - 1): 0].name, $scope.target.parentNode._id, function(check) {
             if(!check.result && number!== 0)
                 return;
