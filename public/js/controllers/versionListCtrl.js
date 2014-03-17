28456709096cd88b153278a3ce922b34ceec0035
Akaash Gupta <akaash.gupta@autodesk.com>
2014-03-17 15:48:05 +0800

    Started work on Diff between 2 versions. Shelved for now.

--- public/js/controllers/versionListCtrl.js
+++ public/js/controllers/versionListCtrl.js
@@ -1,8 +1,9 @@
 'use strict';
 
 angular.module('ace.schematic')
-.controller('versionListCtrl', ['formValidation','$scope', 'SchematicsAPI', '$modalInstance', 'target', function (formValidation, $scope, SchematicsAPI, $modalInstance, target) {
+.controller('versionListCtrl', ['$modal', 'formValidation','$scope', 'SchematicsAPI', '$modalInstance', 'target', function ($modal, formValidation, $scope, SchematicsAPI, $modalInstance, target) {
 
+    $scope.hide = false;
     $scope.target = target;
     $scope.formValidator = formValidation;
     $scope.init = function(){
@@ -33,9 +34,23 @@ angular.module('ace.schematic')
         });
     };
 
+    $scope.launchCompareModal = function() {
+        $scope.hide = true;
+        var modalInstance = $modal.open({
+            templateUrl: 'views/Schematics/CompareVersionModal.html',
+            controller:'CompareVersionModalCtrl',
+            backdrop: 'static',
+        });
+        modalInstance.result.then(function(decision){
+            if(decision && typeof decision === 'number'){
+                $scope.publishComponent(decision);
+            }
+            $scope.hide = false;
+        });
+    };
+
     $scope.chooseHoverTarget = function(index){
         $scope.hoverTgt = index;
-        console.log($scope.hoverTgt);
     };
 
     $scope.cancel = function(){
