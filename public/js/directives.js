d9830f1409cc5fd8722bb4d57d36141cd56d2432
Akaash Gupta <akaash.gupta@autodesk.com>
2014-03-25 15:22:44 +0800

    Fixed bug in directive.

--- public/js/directives.js
+++ public/js/directives.js
@@ -16,26 +16,29 @@ angular.module('ace').directive('resetForm',
 				});
 			}
 		};
-	}).directive('transformLink', function() {
+	}).directive('transformLink', [function() {
 		return {
 			restrict: 'A',
-			link: function(scope, element) {
+			link: function(scope, element, attr) {
 				scope.$watch('cols.length', function () {
-					console.log(element);
-					if(element.context.textContent.indexOf('http://') === 0)
+					if(attr.transformLink.indexOf('http://') === 0)
 					{
-						var link = element.context.textContent;
+						var link = attr.transformLink;
 						element.text('');
 						element.html('<a href="'+link+'"><i class="fa fa-external-link"></i>');
 					}
-					else if(element.context.textContent.indexOf('www.') > -1)
+					else if(attr.transformLink.indexOf('www.') > -1)
 					{
-						var unmodified_link = element.context.textContent;
+						var unmodified_link = attr.transformLink;
 						var modified_link = 'http://' + unmodified_link.substring(unmodified_link.indexOf('www.'));
 						element.text('');
 						element.html('<a href="'+modified_link+'"><i class="fa fa-external-link"></i>');
 					}
+					else
+					{
+						element.text(attr.transformLink);
+					}
 				});
 			}
 		};
-	});
+	}]);
