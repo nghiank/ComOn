e22333fbd3fdf097125880d5e957dee0c3b8bfd8
Akaash Gupta <akaash.gupta@autodesk.com>
2014-04-02 20:44:13 +0800

    Changed environment variables.

--- config/env/production.js
+++ config/env/production.js
@@ -1,16 +1,16 @@
 module.exports = {
-    base_url: 'http://ace-online.autodesk.com:3000',
+    base_url: 'http://comon.herokuapp.com',
     db: "mongodb://root:Iceman123@lennon.mongohq.com:10025/app23651433",
     app: {
         name: "Autodesk - ACE ComOn Production"
     },
     oauth: {
-        requestTokenURL: 'https://accounts.autodesk.com/OAuth/RequestToken',
-        accessTokenURL: 'https://accounts.autodesk.com/OAuth/AccessToken',
+        requestTokenURL: 'https://accounts-dev.autodesk.com/OAuth/RequestToken',
+        accessTokenURL: 'https://accounts-dev.autodesk.com/OAuth/AccessToken',
         userAuthorizationURL: 'http://accounts-dev.autodesk.com/OAuth/Authorize',
         consumerKey: '5f7de223-2148-479b-9ae1-e835f590c117',
         consumerSecret: 'fb3d2f26-d89e-4ab5-9da4-d9c0664c3c9d',
-        callbackURL: 'http://ace-online.autodesk.com:3000/auth/oauth/callback',
-        apiURL: 'https://accounts.autodesk.com'
+        callbackURL: 'http://comon.herokuapp.com/auth/oauth/callback',
+        apiURL: 'https://accounts-dev.autodesk.com'
     }
 }
