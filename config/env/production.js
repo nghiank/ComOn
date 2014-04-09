21f4e5fed935cc5e82706f8403ed119a7c853d49
Akaash Gupta <akaash.gupta@autodesk.com>
2014-04-09 16:31:55 +0800

    Created new comon App with shard symbols.

--- config/env/production.js
+++ config/env/production.js
@@ -1,6 +1,6 @@
 module.exports = {
-    base_url: 'http://comon.herokuapp.com',
-    db: "mongodb://root:Iceman123@lennon.mongohq.com:10025/app23651433",
+    base_url: 'http://comon2.herokuapp.com',
+    db: "mongodb://root:Iceman123@@oceanic.mongohq.com:10062/app23926808",
     app: {
         name: "Autodesk - ACE ComOn Production"
     },
@@ -10,7 +10,7 @@ module.exports = {
         userAuthorizationURL: 'http://accounts-dev.autodesk.com/OAuth/Authorize',
         consumerKey: '5f7de223-2148-479b-9ae1-e835f590c117',
         consumerSecret: 'fb3d2f26-d89e-4ab5-9da4-d9c0664c3c9d',
-        callbackURL: 'http://comon.herokuapp.com/auth/oauth/callback',
+        callbackURL: 'http://comon2.herokuapp.com/auth/oauth/callback',
         apiURL: 'https://accounts-dev.autodesk.com'
     }
 }
