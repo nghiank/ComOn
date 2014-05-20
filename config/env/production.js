77842e1a0d6ccdacfae6d5bd381e65493383051c
Akaash Gupta <akaash.gupta@autodesk.com>
2014-05-20 16:07:23 +0800

    Changed production callback and base URL to https

--- config/env/production.js
+++ config/env/production.js
@@ -1,5 +1,5 @@
 module.exports = {
-    base_url: 'http://comon2.herokuapp.com',
+    base_url: 'https://comon2.herokuapp.com',
     db: "mongodb://root:Iceman123@oceanic.mongohq.com:10062/app23926808",
     app: {
         name: "Autodesk - ACE ComOn Production"
@@ -7,10 +7,10 @@ module.exports = {
     oauth: {
         requestTokenURL: 'https://accounts-dev.autodesk.com/OAuth/RequestToken',
         accessTokenURL: 'https://accounts-dev.autodesk.com/OAuth/AccessToken',
-        userAuthorizationURL: 'http://accounts-dev.autodesk.com/OAuth/Authorize',
+        userAuthorizationURL: 'https://accounts-dev.autodesk.com/OAuth/Authorize',
         consumerKey: '5f7de223-2148-479b-9ae1-e835f590c117',
         consumerSecret: 'fb3d2f26-d89e-4ab5-9da4-d9c0664c3c9d',
-        callbackURL: 'http://comon2.herokuapp.com/auth/oauth/callback',
+        callbackURL: 'https://comon2.herokuapp.com/auth/oauth/callback',
         apiURL: 'https://accounts-dev.autodesk.com'
     }
 }
